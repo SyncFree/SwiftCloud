ac3afa50a460e1d3cecaa16c4ac432f6a00115ff
mzawirsk <mzawirsk@049c2a90-bdf1-4f83-8191-3719d539f8e0>
2012-03-12 03:21:34 +0000

    CRDTIdentifier: equals/hashCode
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/concordant/sources/swiftcloud/java/SwiftCloud@279 049c2a90-bdf1-4f83-8191-3719d539f8e0

--- src/swift/crdt/CRDTIdentifier.java
+++ src/swift/crdt/CRDTIdentifier.java
@@ -13,15 +13,38 @@ public class CRDTIdentifier {
     private final String key;
     
     public CRDTIdentifier(String table, String key) {
+        if (table == null || key == null)
+            throw new NullPointerException("uid cannot have null table or key");
         this.table = table;
         this.key = key;
     }
 
+    /**
+     * @return table for an object; never null
+     */
     public String getTable() {
         return this.table;
     }
 
+    /**
+     * @return key for an object; never null
+     */
     public String getKey() {
         return this.key;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        return prime * table.hashCode() + key.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof CRDTIdentifier)) {
+            return false;
+        }
+        CRDTIdentifier other = (CRDTIdentifier) obj;
+        return table.equals(other.table) && key.equals(other.key);
     }
 }
