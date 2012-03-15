be3fcacb51c3cae30022fabcc51ee5efa0ad30f8
mzawirsk <mzawirsk@049c2a90-bdf1-4f83-8191-3719d539f8e0>
2012-03-15 17:09:00 +0000

    Protocol - FetchVersion: introduce status code and pruneClock
    Fetch can actually fail to find the requested version, thus we need an
    explicit status code for this case, which is different from failing to
    find object
     In order to make use of this kind of error pruneClock information is essential.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/concordant/sources/swiftcloud/java/SwiftCloud@309 049c2a90-bdf1-4f83-8191-3719d539f8e0

--- src/swift/client/proto/FetchObjectVersionReply.java
+++ src/swift/client/proto/FetchObjectVersionReply.java
@@ -12,24 +12,53 @@ import sys.net.api.rpc.RpcMessage;
  * @author mzawirski
  */
 public class FetchObjectVersionReply implements RpcMessage {
+    public enum FetchStatus {
+        /**
+         * The reply contains requested version.
+         */
+        OK,
+        /**
+         * The requested object is not in the store.
+         */
+        OBJECT_NOT_FOUND,
+        /**
+         * The requested version of an object is not any more available in the
+         * store.
+         */
+        VERSION_NOT_FOUND
+    }
+
+    protected FetchStatus status;
     // TODO: shalln't we use CRDT class simply and allow leaving certain fields
     // null?
     protected CRDT<?> crdt;
     protected CausalityClock version;
+    protected CausalityClock pruneClock;;
 
     // Fake constructor for Kryo serialization. Do NOT use.
     public FetchObjectVersionReply() {
     }
 
-    public FetchObjectVersionReply(CRDT<?> crdt, CausalityClock version) {
+    public FetchObjectVersionReply(FetchStatus status, CRDT<?> crdt, CausalityClock version, CausalityClock pruneClock) {
+        this.status = status;
         this.crdt = crdt;
         this.version = version;
+        this.pruneClock = pruneClock;
     }
 
     /**
-     * @return state of an object requested by the client, pruned from history
-     *         at most to the level specified by version in the original client
-     *         request; null if {@link #isFound()} returns false
+     * @return status code of the reply
+     */
+    public FetchStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * @return state of an object requested by the client; if
+     *         {@link #getStatus()} is {@link FetchStatus#OK} then the object is
+     *         pruned from history at most to the level specified by version in
+     *         the original client request; null if {@link #getStatus()} is
+     *         {@link FetchStatus#OBJECT_NOT_FOUND}.
      */
     public CRDT<?> getCrdt() {
         return crdt;
@@ -37,19 +66,19 @@ public class FetchObjectVersionReply implements RpcMessage {
 
     /**
      * @return version of an object returned, possibly higher than the version
-     *         requested by the client; if {@link #isFound()} returns true, this
-     *         represents the latest clock known when object was absent in the
-     *         store
+     *         requested by the client; null if {@link #getStatus()} is
+     *         {@link FetchStatus#OBJECT_NOT_FOUND}
      */
     public CausalityClock getVersion() {
         return version;
     }
 
     /**
-     * @return true if object was found in the store, false otherwise
+     * @return pruneClock of an object returned; null if {@link #getStatus()} is
+     *         {@link FetchStatus#OBJECT_NOT_FOUND}
      */
-    public boolean isFound() {
-        return crdt == null;
+    public CausalityClock getPruneClock() {
+        return version;
     }
 
     @Override
