242c356ab7e1a00b1031fb09610c5fad42bb9bb6
abieniusa <abieniusa@049c2a90-bdf1-4f83-8191-3719d539f8e0>
2012-03-30 10:32:01 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/concordant/sources/swiftcloud/java/SwiftCloud@358 049c2a90-bdf1-4f83-8191-3719d539f8e0

--- src/swift/application/LocalSetUpTest.java
+++ src/swift/application/LocalSetUpTest.java
@@ -1,7 +1,17 @@
 package swift.application;
 
+import static sys.net.api.Networking.Networking;
+import swift.client.SwiftImpl;
+import swift.crdt.CRDTIdentifier;
+import swift.crdt.IntegerTxnLocal;
+import swift.crdt.interfaces.CachePolicy;
+import swift.crdt.interfaces.TxnHandle;
+import swift.dc.DCConstants;
 import swift.dc.DCSequencerServer;
 import swift.dc.DCServer;
+import sys.Sys;
+import sys.net.api.Endpoint;
+import sys.net.api.rpc.RpcEndpoint;
 
 /**
  * Local setup/test with one server and two clients.
@@ -11,9 +21,6 @@ import swift.dc.DCServer;
  */
 public class LocalSetUpTest {
     static String sequencerName = "localhost";
-
-    static String client1Name = "c1";
-    static String client2Name = "c2";
 
     public static void main(String[] args) {
         Thread sequencer = new Thread() {
@@ -31,5 +38,39 @@ public class LocalSetUpTest {
             }
         };
         server.start();
+
+        for (int i = 0; i < 2; i++) {
+            final int portId = i + 2000;
+            Thread client = new Thread() {
+                public void run() {
+                    Sys.init();
+                    RpcEndpoint localEP = Networking.rpcBind(portId, null);
+                    final Endpoint serverEP = Networking.resolve("localhost", DCConstants.SURROGATE_PORT);
+                    SwiftImpl clientServer = new SwiftImpl(localEP, serverEP);
+                    clientCode(clientServer);
+                }
+
+            };
+            client.start();
+        }
+    }
+
+    private static void clientCode(SwiftImpl server) {
+        try {
+            TxnHandle handle = server.beginTxn(CachePolicy.STRICTLY_MOST_RECENT, false);
+            IntegerTxnLocal i1 = handle.get(new CRDTIdentifier("e", "1"), false, swift.crdt.IntegerVersioned.class);
+            System.out.println("(e,1) = " + i1.getValue());
+            IntegerTxnLocal i2 = handle.get(new CRDTIdentifier("e", "2"), false, swift.crdt.IntegerVersioned.class);
+            System.out.println("(e,2) = " + i2.getValue());
+            i1.add(1);
+            System.out.println("(e,1).add(1)");
+            System.out.println("(e,1) = " + i1.getValue());
+            handle.commit();
+            System.out.println("commit");
+
+            System.out.println("TetsClient ended with success");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
