7503c72b40292da89816a60503611daa778f5a0a
abieniusa <abieniusa@049c2a90-bdf1-4f83-8191-3719d539f8e0>
2012-05-01 15:25:01 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/concordant/sources/swiftcloud/java/SwiftCloud@641 049c2a90-bdf1-4f83-8191-3719d539f8e0

--- scripts/ec2-deploy.sh
+++ scripts/ec2-deploy.sh
@@ -2,7 +2,12 @@
 # ec2-deploy <server1 server2 ...>
 . scripts/ec2-common.sh
 
-servers=$*
+if [ -z "$*" ]; then
+  echo "no servers specified on commandline, taking $EC2_ALL"
+  servers=$EC2_ALL
+else
+  servers=$*
+fi
 
 if [ ! -f $JAR ]; then
 	echo "$JAR not found" && exit 1
