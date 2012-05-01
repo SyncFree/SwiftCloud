99a1f04f642a2ad6f362500263b1a1fbf1224259
mzawirsk <mzawirsk@049c2a90-bdf1-4f83-8191-3719d539f8e0>
2012-05-01 03:12:21 +0000

    script
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/concordant/sources/swiftcloud/java/SwiftCloud@627 049c2a90-bdf1-4f83-8191-3719d539f8e0

--- scripts/ec2-kill.sh
+++ scripts/ec2-kill.sh
@@ -5,7 +5,7 @@
 # stop <server1 server2 ...>
 stop() {
         servers=$*
-        echo "Stopping swift servers: $servers"
+        echo "Stopping swift apps: $servers"
         for server in $servers; do
                 kill_swift $server
         done
