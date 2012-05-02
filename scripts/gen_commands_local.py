ae17adcc5780799372e0a1e195c43331cbd69e49
mzawirsk <mzawirsk@049c2a90-bdf1-4f83-8191-3719d539f8e0>
2012-05-02 23:54:09 +0000

    Social workload: only some users are active
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/concordant/sources/swiftcloud/java/SwiftCloud@729 049c2a90-bdf1-4f83-8191-3719d539f8e0

--- scripts/gen_commands_local.py
+++ scripts/gen_commands_local.py
@@ -36,21 +36,22 @@ def doMixedLine(user):
   return 0
 
 # A mixed load
-def doMixed(friends_per_user, commands_per_user_biased, commands_per_user, total_commands, number_of_sites):
+def doMixed(friends_per_user, commands_per_user_biased, commands_per_user, total_commands, number_of_sites, active_users):
   mix = []
   
   print "Number of sites: " + str(number_of_sites)
   for site in range(0,number_of_sites):
     print "generating output for site " + str(site)
-    if len(sys.argv) == 8:
-          outFile = sys.argv[7]+"-"+str(site)
+    if len(sys.argv) == 9:
+          outFile = sys.argv[8]+"-"+str(site)
           fOut = open(outFile, 'w')
     else:
           fOut = sys.stdout
 
+    activeSessionsPerSite = active_users/number_of_sites
     partitionSize = len(allUsers)/number_of_sites
     print "Size of user partition: "+ str(partitionSize)
-    for sessions in range(0,partitionSize): 
+    for sessions in range(0,activeSessionsPerSite): 
         userName = allUsers[random.randint(0,partitionSize-1) + partitionSize * site]
     
     #generate the friends for that user
@@ -92,8 +93,8 @@ def readUsers(usersFile):
 
 def main():
   numUsers=0
-  if (len(sys.argv) != 7 and len(sys.argv) != 8):
-    print "Usage ", sys.argv[0],"<users_file> <number_of_friends> <ops_per_session_biased> <ops_per_session_random> <totoal_ops> <num_sites> [<out_file>]"
+  if (len(sys.argv) != 8 and len(sys.argv) != 9):
+    print "Usage ", sys.argv[0],"<users_file> <number_of_friends> <ops_per_session_biased> <ops_per_session_random> <totoal_ops> <num_sites> <active_users> [<out_file>]"
     sys.exit()
   
   fName = sys.argv[1]
@@ -102,13 +103,14 @@ def main():
   commands_per_user = int(sys.argv[4])
   total_commands = int(sys.argv[5])
   number_of_sites = int(sys.argv[6])
+  active_users = int(sys.argv[7])
 
 
   print "sites: " + str(number_of_sites)
 
   random.seed(time.time())
   readUsers(fName)
-  doMixed(friends_per_user, commands_per_user_biased, commands_per_user, total_commands, number_of_sites)
+  doMixed(friends_per_user, commands_per_user_biased, commands_per_user, total_commands, number_of_sites, active_users)
 
 if __name__ == "__main__":
   main()
