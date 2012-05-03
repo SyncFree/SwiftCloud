5aea2682dd7a07609be98b3505e37e5fd9fe6852
mzawirsk <mzawirsk@049c2a90-bdf1-4f83-8191-3719d539f8e0>
2012-05-03 17:05:33 +0000

    Make ec2-ping universal
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/concordant/sources/swiftcloud/java/SwiftCloud@782 049c2a90-bdf1-4f83-8191-3719d539f8e0

--- scripts/ec2-ping.sh
+++ scripts/ec2-ping.sh
@@ -2,18 +2,12 @@
 # ec2-run <server> <class> <args>
 . scripts/ec2-common.sh
 
+machines=$*
+for i in $machines; do
+	# do not repide two side!!
+	for j in $machines; do
+		PING="ping -c 5 $j"
+		run_cmd "$i" "$PING" > results/pingtime_"$i"_"$j".log
+	done
+done
 
-server1=$EC2_ASIA_SINGAPORE1
-server2=$EC2_ASIA_TOKYO
-PING="ping -c 10 $server2"
-run_cmd "$server1" "$PING" > results/pingtime_"$server1"_"$server2".log
-
-server1=$EC2_US_NORTHCALIFORNIA1
-server2=$EC2_US_OREGON
-PING="ping -c 10 $server2"
-run_cmd "$server1" "$PING" > results/pingtime_"$server1"_"$server2".log
-
-server1=$EC2_US_NORTHCALIFORNIA1
-server2=$EC2_ASIA_TOKYO
-PING="ping -c 10 $server2"
-run_cmd "$server1" "$PING" > results/pingtime_"$server1"_"$server2".log
