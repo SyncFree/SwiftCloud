795bfb23f939aaf4864c04b0cbd574ac71c04b30
abieniusa <abieniusa@049c2a90-bdf1-4f83-8191-3719d539f8e0>
2012-09-19 14:15:20 +0000

    Added comments to BaseCRDTTxnLocal.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/concordant/sources/swiftcloud/java/SwiftCloud@1098 049c2a90-bdf1-4f83-8191-3719d539f8e0

--- src/swift/crdt/BaseCRDTTxnLocal.java
+++ src/swift/crdt/BaseCRDTTxnLocal.java
@@ -7,11 +7,42 @@ import swift.crdt.interfaces.CRDTUpdate;
 import swift.crdt.interfaces.TxnHandle;
 import swift.crdt.interfaces.TxnLocalCRDT;
 
+/**
+ * Base class for implementations of transaction-local views for CRDT objects of
+ * the interface {@TxnLocalCRDT}.
+ * 
+ * This class provides and manages the generic information needed for local
+ * queries and updates in the context of a {@TxnHandle}.
+ * 
+ * @author annettebieniusa, mzawirsk
+ * 
+ * @param <V>
+ *            type implementing the CRDT interface
+ */
+
 public abstract class BaseCRDTTxnLocal<V extends CRDT<V>> implements TxnLocalCRDT<V> {
+    // handle to transaction within which the object is created
     private final TxnHandle txn;
+    // identifier of CRDT from which the object is derived
     protected final CRDTIdentifier id;
+    // snapshot clock of the object, reflecting the snapshot
     private final CausalityClock clock;
 
+    /**
+     * Constructor for the base representation of transaction-local objects
+     * views.
+     * 
+     * @param id
+     *            identifier of the CRDT object from which this txn-local view
+     *            is derived
+     * @param txn
+     *            transaction with which the object is registered
+     * @param clock
+     *            snapshot clock of the object
+     * @param creationState
+     *            initial state of the parent if it created within the current
+     *            transaction
+     */
     public BaseCRDTTxnLocal(CRDTIdentifier id, TxnHandle txn, CausalityClock clock, V creationState) {
         this.txn = txn;
         this.id = id;
@@ -26,10 +57,21 @@ public abstract class BaseCRDTTxnLocal<V extends CRDT<V>> implements TxnLocalCRD
         return this.txn;
     }
 
+    /**
+     * Gives out timestamps for updates.
+     * 
+     * @return local timestamp under which the next operation can be registered
+     */
     protected TripleTimestamp nextTimestamp() {
         return getTxnHandle().nextTimestamp();
     }
 
+    /**
+     * Registers a new update for the CRDT from which the local view is derived
+     * as part of the transaction.
+     * 
+     * @param op
+     */
     protected void registerLocalOperation(final CRDTUpdate<V> op) {
         getTxnHandle().registerOperation(this.id, op);
     }
