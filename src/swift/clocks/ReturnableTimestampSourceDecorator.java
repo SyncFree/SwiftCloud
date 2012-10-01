83de65dac73a6663c2f8cd8a950e212f134cb05c
mzawirsk <mzawirsk@049c2a90-bdf1-4f83-8191-3719d539f8e0>
2012-10-01 20:31:51 +0000

    ReturnableTimestampSourceDecorator to avoid holes in VVs
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/concordant/sources/swiftcloud/java/SwiftCloud-branches/1pc@1162 049c2a90-bdf1-4f83-8191-3719d539f8e0

---
+++ src/swift/clocks/ReturnableTimestampSourceDecorator.java
@@ -0,0 +1,33 @@
+package swift.clocks;
+
+/**
+ * TimestampSource decorator that allows to explicitly return and later reuse
+ * last generated timestamp. Useful for generating continuous sequences for
+ * CausalityClock when some timestamps are not included in the CausalityClock -
+ * they can be returned to the generator instead.
+ * 
+ * @author mzawirski
+ */
+public class ReturnableTimestampSourceDecorator<T extends Timestamp> implements TimestampSource<T> {
+    private final TimestampSource<T> origSource;
+    private boolean lastTimestampReturned;
+    private T lastTimestamp;
+
+    public ReturnableTimestampSourceDecorator(TimestampSource<T> origSource) {
+        this.origSource = origSource;
+    }
+
+    @Override
+    public T generateNew() {
+        if (lastTimestampReturned) {
+            lastTimestampReturned = false;
+            return lastTimestamp;
+        }
+        lastTimestamp = origSource.generateNew();
+        return lastTimestamp;
+    }
+
+    public void returnLastTimestamp() {
+        lastTimestampReturned = false;
+    }
+}
