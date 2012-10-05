c39254efd5d93f86316c3523cbec30cd22b45be8
mzawirsk <mzawirsk@049c2a90-bdf1-4f83-8191-3719d539f8e0>
2012-10-05 18:24:04 +0000

    [stephanemartin, merge-from-trunk] add MaxCVTxnLocal
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/concordant/sources/swiftcloud/java/SwiftCloud-branches/1pc@1269 049c2a90-bdf1-4f83-8191-3719d539f8e0

---
+++ src/swift/crdt/filesystem/MaxVCTxnLocal.java
@@ -0,0 +1,55 @@
+/*
+ *  Replication Benchmarker
+ *  https://github.com/score-team/replication-benchmarker/
+ *  Copyright (C) 2012 LORIA / Inria / SCORE Team
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+package swift.crdt.filesystem;
+
+import swift.clocks.CausalityClock;
+import swift.crdt.BaseCRDTTxnLocal;
+import swift.crdt.CRDTIdentifier;
+import swift.crdt.interfaces.CRDT;
+import swift.crdt.interfaces.CRDTQuery;
+import swift.crdt.interfaces.TxnHandle;
+
+/**
+ *
+ * @author Stephane Martin <stephane.martin@loria.fr>
+ */
+public class MaxVCTxnLocal extends BaseCRDTTxnLocal {
+
+    public MaxVCTxnLocal(CausalityClock vc, CRDTIdentifier id, TxnHandle txn, CausalityClock clock, CRDT creationState) {
+        super(id, txn, clock, creationState);
+        this.vc = vc;
+    }
+
+    
+    
+    CausalityClock vc;
+    @Override
+    public CausalityClock getValue() {
+        return vc;
+    }
+    public void setValue(CausalityClock vc){
+        this.vc=vc;
+    }
+    @Override
+    public Object executeQuery(CRDTQuery query) {
+        return query.executeAt(this);
+    }
+
+}
