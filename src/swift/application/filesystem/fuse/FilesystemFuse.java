892af1b02c328822e7e5ac8071162dfae3d6c3f9
mzawirsk <mzawirsk@049c2a90-bdf1-4f83-8191-3719d539f8e0>
2012-10-12 05:39:43 +0000

    [abieniusa/merge-from-trunk]
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/concordant/sources/swiftcloud/java/SwiftCloud-branches/1pc@1477 049c2a90-bdf1-4f83-8191-3719d539f8e0

--- src/swift/application/filesystem/fuse/FilesystemFuse.java
+++ src/swift/application/filesystem/fuse/FilesystemFuse.java
@@ -306,6 +306,9 @@ public class FilesystemFuse implements Filesystem3, XattrSupport {
             } catch (VersionNotFoundException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
             txn.rollback();
         }
@@ -345,6 +348,9 @@ public class FilesystemFuse implements Filesystem3, XattrSupport {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
