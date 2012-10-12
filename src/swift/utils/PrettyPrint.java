edbbc0037b5c7a93cfad8ec4cbe2b9ce725ab486
mzawirsk <mzawirsk@049c2a90-bdf1-4f83-8191-3719d539f8e0>
2012-10-12 05:39:25 +0000

    [abieniusa/merge-from-trunk] Fix in PrettyPrint util class. Now more general, as it accepts collections (not only sets).
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/concordant/sources/swiftcloud/java/SwiftCloud-branches/1pc@1475 049c2a90-bdf1-4f83-8191-3719d539f8e0

--- src/swift/utils/PrettyPrint.java
+++ src/swift/utils/PrettyPrint.java
@@ -1,27 +1,12 @@
 package swift.utils;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 
 public final class PrettyPrint {
     private PrettyPrint() {
-    }
-
-    public static <K> String printSet(String start, String end, String sep, Set<K> elems) {
-        StringBuffer buf = new StringBuffer();
-        buf.append(start);
-        Iterator<K> it = elems.iterator();
-        while (it.hasNext()) {
-            K e = it.next();
-            buf.append(e);
-            if (it.hasNext()) {
-                buf.append(sep);
-            }
-        }
-        buf.append(end);
-        return buf.toString();
     }
 
     public static <K, V> String printMap(String start, String end, String sep, String map, Map<K, V> elems) {
@@ -31,13 +16,28 @@ public final class PrettyPrint {
         while (it.hasNext()) {
             Entry<K, V> e = it.next();
             buf.append(e.getKey());
-            buf.append(" -> ");
+            buf.append(map);
             buf.append(e.getValue());
             if (it.hasNext()) {
-                buf.append("; ");
+                buf.append(sep);
             }
         }
-        buf.append(" }");
+        buf.append(end);
+        return buf.toString();
+    }
+
+    public static <K, V> String printCollection(String start, String end, String sep, Collection<V> elems) {
+        StringBuffer buf = new StringBuffer();
+        buf.append(start);
+        Iterator<V> it = elems.iterator();
+        while (it.hasNext()) {
+            V e = it.next();
+            buf.append(e);
+            if (it.hasNext()) {
+                buf.append(sep);
+            }
+        }
+        buf.append(end);
         return buf.toString();
     }
 }
