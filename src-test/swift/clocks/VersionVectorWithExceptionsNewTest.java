793a1cc2dd5aac48e1a45a2fce5a4a3952de0ac7
preguica <preguica@049c2a90-bdf1-4f83-8191-3719d539f8e0>
2012-10-14 10:07:25 +0000

    tests for intersect
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/concordant/sources/swiftcloud/java/SwiftCloud-branches/1pc@1541 049c2a90-bdf1-4f83-8191-3719d539f8e0

--- src-test/swift/clocks/VersionVectorWithExceptionsNewTest.java
+++ src-test/swift/clocks/VersionVectorWithExceptionsNewTest.java
@@ -19,6 +19,9 @@ public class VersionVectorWithExceptionsNewTest {
     private Timestamp tsA5;
     private Timestamp tsB1;
     private Timestamp tsB2;
+    private Timestamp tsB3;
+    private Timestamp tsB4;
+    private Timestamp tsB5;
 
     @Before
     public void setUp() {
@@ -34,6 +37,9 @@ public class VersionVectorWithExceptionsNewTest {
         tsA5 = siteAGen.generateNew();
         tsB1 = siteBGen.generateNew();
         tsB2 = siteBGen.generateNew();
+        tsB3 = siteBGen.generateNew();
+        tsB4 = siteBGen.generateNew();
+        tsB5 = siteBGen.generateNew();
     }
 
     @Test
@@ -252,6 +258,96 @@ public class VersionVectorWithExceptionsNewTest {
         clock3.record(tsA3);
 
         assertTrue(clock.merge(clock2) == CMP_CLOCK.CMP_DOMINATES);
+        assertTrue(clock.compareTo(clock3) == CMP_CLOCK.CMP_EQUALS);
+    }
+
+    @Test
+    public void testAddInDifferentOrder() {
+        // Prepare [4 \ {2}, 0] vector.
+        clock.record(tsA1);
+        clock.record(tsA3);
+        clock.record(tsA4);
+        // Prepare [4 \ {2}, 0] vector.
+        clock2.record(tsA1);
+        clock2.record(tsA4);
+        clock2.record(tsA3);
+        // Prepare [4 \ {2}, 0] vector.
+        clock3.record(tsA4);
+        clock3.record(tsA3);
+        clock3.record(tsA1);
+
+        assertTrue(clock.compareTo(clock2) == CMP_CLOCK.CMP_EQUALS);
+        assertTrue(clock.compareTo(clock3) == CMP_CLOCK.CMP_EQUALS);
+        assertTrue(clock2.compareTo(clock3) == CMP_CLOCK.CMP_EQUALS);
+    }
+
+    @Test
+    public void testIntersect1() {
+        // Prepare [4 \ {2}, 0] vector.
+        clock.record(tsA1);
+        clock.record(tsA3);
+        clock.record(tsA4);
+        // Prepare[2\{1}, 0] vector.
+        clock2.record(tsA2);
+        // Prepare[0, 0] vector.
+ 
+        assertTrue(clock.intersect(clock2) == CMP_CLOCK.CMP_CONCURRENT);
+        assertTrue(clock.compareTo(clock3) == CMP_CLOCK.CMP_EQUALS);
+    }
+    @Test
+    public void testIntersect2() {
+        // Prepare [4 \ {2}, 0] vector.
+        clock.record(tsA1);
+        clock.record(tsA3);
+        clock.record(tsA4);
+        // Prepare[4\{1,3}, 0] vector.
+        clock2.record(tsA2);
+        clock2.record(tsA4);
+        // Prepare[4\{1,2,3}, 0] vector.
+        clock3.record(tsA4);
+ 
+        assertTrue(clock.intersect(clock2) == CMP_CLOCK.CMP_CONCURRENT);
+        assertTrue(clock.compareTo(clock3) == CMP_CLOCK.CMP_EQUALS);
+    }
+    @Test
+    public void testIntersect3() {
+        // Prepare [4 \ {2}, 1] vector.
+        clock.record(tsA1);
+        clock.record(tsA3);
+        clock.record(tsA4);
+        clock.record(tsB1);
+        // Prepare[4\{1,3}, 1] vector.
+        clock2.record(tsA2);
+        clock2.record(tsA4);
+        clock2.record(tsB1);
+        // Prepare[4\{1,2,3}, 1] vector.
+        clock3.record(tsA4);
+        clock3.record(tsB1);
+ 
+        assertTrue(clock.intersect(clock2) == CMP_CLOCK.CMP_CONCURRENT);
+        assertTrue(clock.compareTo(clock3) == CMP_CLOCK.CMP_EQUALS);
+    }
+    @Test
+    public void testIntersect4() {
+        // Prepare [3, 2] vector.
+        clock.record(tsA1);
+        clock.record(tsA2);
+        clock.record(tsA3);
+        clock.record(tsB1);
+        clock.record(tsB2);
+        // Prepare[2, 3] vector.
+        clock2.record(tsA1);
+        clock2.record(tsA2);
+        clock2.record(tsB1);
+        clock2.record(tsB2);
+        clock2.record(tsB3);
+        // Prepare[2, 2] vector.
+        clock3.record(tsA1);
+        clock3.record(tsA2);
+        clock3.record(tsB1);
+        clock3.record(tsB2);
+ 
+        assertTrue(clock.intersect(clock2) == CMP_CLOCK.CMP_CONCURRENT);
         assertTrue(clock.compareTo(clock3) == CMP_CLOCK.CMP_EQUALS);
     }
 }
