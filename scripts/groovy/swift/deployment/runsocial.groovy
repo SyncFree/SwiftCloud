7285776a7e5871b8a5f282cde6040210d5eafc4d
Marek Zawirski <marek.zawirski@lip6.fr>
2014-05-15 20:44:04 +0200

    scripts updated to new EC2 configuration

--- scripts/groovy/swift/deployment/runsocial.groovy
+++ scripts/groovy/swift/deployment/runsocial.groovy
@@ -12,14 +12,18 @@ def __ = onControlC({
 
 
 Surrogates = [
-    'ec2-54-228-122-250.eu-west-1.compute.amazonaws.com',
-    'ec2-54-244-165-15.us-west-2.compute.amazonaws.com',
-    'ec2-54-234-203-38.compute-1.amazonaws.com'
+    'ec2-54-76-29-118.eu-west-1.compute.amazonaws.com',
+    'ec2-54-187-177-232.us-west-2.compute.amazonaws.com',
+    'ec2-107-21-136-93.compute-1.amazonaws.com',
 ]
 
 
 //    Scouts = (PlanetLab_EU).unique()
-Scouts = (PlanetLab_NC + PlanetLab_NV + PlanetLab_EU).unique()
+// (PlanetLab_NC + PlanetLab_NV + PlanetLab_EU).unique()
+Scouts = ['ec2-54-76-28-141.eu-west-1.compute.amazonaws.com',
+          'ec2-54-187-210-164.us-west-2.compute.amazonaws.com',
+          'ec2-54-227-32-176.compute-1.amazonaws.com',
+]
 
 ShepardAddr = Surrogates.get(0);
 
@@ -48,18 +52,19 @@ deployTo(AllMachines, SwiftSocial_Props)
 
 def shep = SwiftSocial.runShepard( ShepardAddr, Duration, "Released" )
 
-SwiftSocial.runEachAsDatacentre(Surrogates, "256m", "3096m")
-Sleep(10)
+SwiftSocial.runEachAsDatacentre(Surrogates, "256m", "256m")
 
-//    println "==== WAITING A BIT BEFORE INITIALIZING DB ===="
-//    println "==== INITIALIZING DATABASE ===="
-//    def INIT_DB_DC = Surrogates.get(0)
-//    def INIT_DB_CLIENT = Surrogates.get(0)
-//
-//    SwiftSocial.initDB( INIT_DB_CLIENT, INIT_DB_DC, SwiftSocial_Props)
+Sleep(20)
+println "==== WAITING A BIT BEFORE INITIALIZING DB ===="
+println "==== INITIALIZING DATABASE ===="
+def INIT_DB_DC = Surrogates.get(0)
+def INIT_DB_CLIENT = Surrogates.get(0)
+
+SwiftSocial.initDB( INIT_DB_CLIENT, INIT_DB_DC, SwiftSocial_Props)
 
 
 println "==== WAITING A BIT BEFORE STARTING SCOUTS ===="
+Sleep(20)
 SwiftSocial.runStandaloneScouts( Scouts, Surrogates, SwiftSocial_Props, ShepardAddr, Threads )
 
 println "==== WAITING FOR SHEPARD SIGNAL PRIOR TO COUNTDOWN ===="
