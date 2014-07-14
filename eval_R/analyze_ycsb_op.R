5855cf12feaa2ddef7bc3c0c490fd4683c9338aa
Annette Bieniusa <bieniusa@cs.uni-kl.de>
2014-07-14 23:59:01 +0200

    cutting off begin and end of measurement, throughput plot

--- eval_R/analyze_ycsb_op.R
+++ eval_R/analyze_ycsb_op.R
@@ -4,13 +4,6 @@ require("reshape2");
 require("gridExtra");
 
 
-preprocess_duration <- function (data, start_time, prune_start, duration){
-  #Throw away first 'prune_start' msec and take the next 'duration' msecs
-  data <- subset(data,data$V1 - start_time > prune_start)
-  data <- subset(data,data$V1 - start_time < duration)
-  return (data)
-}
-
 
 preprocess_OP <- function (data){
   #Data format is:
@@ -18,33 +11,33 @@ preprocess_OP <- function (data){
   
   #Filter for batch updates
   data <- subset(data,data$V2=="APP_OP")
+  data <- subset(data,data$V4=="read" | data$V4=="update")
   return (data)
 }
 
 
-path <- "~/data_eval/50reads-50updates/Jun29-1404068293230-c09289b5-DC-1-SU-1-SC-8-TH-4-records-100000-operations-500000/ec2-54-72-34-188.eu-west-1.compute.amazonaws.com/";
-file <- paste(path,"test_ycsb.csv",sep="/")
+toplevel_path <- "~/git/data_eval/50reads-50updates/";
+#file <- paste(path,"test_ycsb.csv",sep="/")
 
 
-toplevel_path <- "/home/zawir/workspace/swiftcloud/results/ycsb/workloada/test/"
-# "~/data_eval/50reads-50updates/"
-
-#file <- "~/data_eval/50reads-50updates/remote-caching/"
+#toplevel_path <- "/home/zawir/workspace/swiftcloud/results/ycsb/workloada/test/"
 
 file_list <- list.files(toplevel_path, pattern="*scout-stdout.log",recursive=TRUE)
+
 file_list <- (paste(toplevel_path,file_list,sep="/"))
 
 p <- list()
 d <- data.frame()
 dc <- 1
 su <- 1
-sc <- 1
-th <- 16
+sc <- 12
+th <- 2
 pruning <- 30000
 notifications <- 1000
 records <- "100000"
 ops <- "1000000"
-j <- grep(paste("DC",dc,"SU",su,"pruning",pruning,"notifications",notifications,"SC",sc,"TH",th,"records",records,"operations",ops,sep="-"),file_list)
+#j <- grep(paste("DC",dc,"SU",su,"pruning",pruning,"notifications",notifications,"SC",sc,"TH",th,"records",records,"operations",ops,sep="-"),file_list)
+j <- grep(paste("DC",dc,"SU",su,"SC",sc,"TH",th,"records",records,"operations",ops,sep="-"),file_list)
 
 #Sort by number of threads
 for (file in file_list[j]){
@@ -56,7 +49,7 @@ for (file in file_list[j]){
 #Set names for columns
 names(d) <- c("timestamp","type","sessionId","operation","duration")
 
-
+helper <- d
 summary(d)
 #Filter out the rows where duration couldn't be parsed due to 
 d <- subset(d,!is.na(d$duration))
@@ -65,7 +58,11 @@ d <- subset(d,!is.na(d$duration))
 # TODO: aggregated throughput computation
 
 start_time <- min(d$timestamp)
-d <- preprocess_duration(d,start_time,1000,30000)
+prune_start <- 10000
+duration_run <- 300000
+d <- subset(d,d$timestamp - start_time > prune_start)
+d <- subset(d,d$timestamp - start_time < duration_run)
+
 
 summary(d)
 
@@ -76,8 +73,8 @@ format_ext <- ".png"
 #Scatterplot for distribution over time
 
 # sampled_subset <- df[sample(nrow(df),n),]
-scatter.plot <- ggplot(d, aes(timestamp,duration)) + geom_point(aes(color=operation))
-ggsave(scatter.plot, file=paste(toplevel_path, "timeline-TH",th,format_ext,collapse=""), scale=1)
+#scatter.plot <- ggplot(d, aes(timestamp,duration)) + geom_point(aes(color=operation))
+#ggsave(scatter.plot, file=paste(toplevel_path, "timeline-TH",th,format_ext,collapse=""), scale=1)
 
 #CDF Plot
 cdf.plot <- ggplot(d, aes(x=duration)) + stat_ecdf(aes(colour=operation)) + ggtitle (paste("TH",th))
@@ -86,7 +83,7 @@ cdf.plot
 #p <- qplot(duration, data = d,binwidth=5,color=operation,geom="freqpoly") + facet_wrap( ~ sessionId)
 
 #Throughput plot
-throughput.plot <- ggplot(d, aes(timestamp,..density..)) + geom_histogramn(binwidth=1000) 
+throughput.plot <- ggplot(d, aes(timestamp,..density..)) + geom_freqpoly(binwidth=1000) 
 throughput.plot
 
 
