49eed2d938e26e149dbfb6ce1018efcd4d4a0f5b
Marek Zawirski <marek.zawirski@gmail.com>
2014-08-07 19:38:07 +0200

    DCServer: comment out strange initialization logic

--- src-core/swift/dc/DCServer.java
+++ src-core/swift/dc/DCServer.java
@@ -97,10 +97,10 @@ public class DCServer {
             }
         }
         // TODO: What is that if statement for???
-        if (!props.containsKey(DCConstants.DATABASE_CLASS)) {
-            // TODO: why should we disable it?
-            props.setProperty(DCConstants.PRUNE_POLICY, "false");
-        }
+        // if (!props.containsKey(DCConstants.DATABASE_CLASS)) {
+        // // TODO: why should we disable it?
+        // props.setProperty(DCConstants.PRUNE_POLICY, "false");
+        // }
 
         props.setProperty(DCConstants.PRUNING_INTERVAL_PROPERTY,
                 Args.valueOf(args, "-pruningMs", DCConstants.DEFAULT_PRUNING_INTERVAL_MS) + "");
