9779faeb60eb5162e87b6b24e053e33035f9d3db
Marek Zawirski <marek.zawirski@gmail.com>
2014-08-18 15:26:45 +0200

    IP addresses update (TODO: move it out of git)

--- scripts/groovy/swift/deployment/topology_multidc1.groovy
+++ scripts/groovy/swift/deployment/topology_multidc1.groovy
@@ -7,37 +7,37 @@ import static swift.deployment.Topology.*;
 // NODES
 EuropeEC2 = [
     // DC only
-    'ec2-54-76-188-118.eu-west-1.compute.amazonaws.com'
+    'ec2-54-77-131-169.eu-west-1.compute.amazonaws.com'
 ]
 
 NVirginiaEC2 = [
     // first node is a DC, followed by two groups of 6 and 7 scouts
-    'ec2-107-23-34-74.compute-1.amazonaws.com',
-    'ec2-54-210-190-244.compute-1.amazonaws.com',
-    'ec2-107-23-34-114.compute-1.amazonaws.com',
-    'ec2-107-23-35-152.compute-1.amazonaws.com',
-    'ec2-107-23-34-194.compute-1.amazonaws.com',
-    'ec2-54-210-189-179.compute-1.amazonaws.com',
-    'ec2-107-21-6-172.compute-1.amazonaws.com',
-    'ec2-107-23-35-19.compute-1.amazonaws.com',
-    'ec2-107-23-4-205.compute-1.amazonaws.com',
-    'ec2-107-23-42-21.compute-1.amazonaws.com',
-    'ec2-54-210-190-61.compute-1.amazonaws.com',
-    'ec2-107-23-42-113.compute-1.amazonaws.com',
-    'ec2-54-236-249-84.compute-1.amazonaws.com',
-    'ec2-54-210-248-126.compute-1.amazonaws.com'
+    'ec2-54-164-126-219.compute-1.amazonaws.com',
+    'ec2-54-164-125-149.compute-1.amazonaws.com',
+    'ec2-54-164-121-180.compute-1.amazonaws.com',
+    'ec2-54-164-122-55.compute-1.amazonaws.com',
+    'ec2-54-164-125-119.compute-1.amazonaws.com',
+    'ec2-54-164-69-240.compute-1.amazonaws.com',
+    'ec2-54-164-122-87.compute-1.amazonaws.com',
+    'ec2-54-164-124-122.compute-1.amazonaws.com',
+    'ec2-54-164-122-158.compute-1.amazonaws.com',
+    'ec2-54-85-20-253.compute-1.amazonaws.com',
+    'ec2-54-164-120-209.compute-1.amazonaws.com',
+    'ec2-54-164-120-104.compute-1.amazonaws.com',
+    'ec2-54-164-88-33.compute-1.amazonaws.com',
+    'ec2-54-164-121-226.compute-1.amazonaws.com'
 ]
 
 OregonEC2 = [
     // first node is a DC, followed by 7 scouts
-    'ec2-54-201-151-156.us-west-2.compute.amazonaws.com',
-    'ec2-54-201-151-159.us-west-2.compute.amazonaws.com',
-    'ec2-54-201-151-204.us-west-2.compute.amazonaws.com',
-    'ec2-54-201-151-196.us-west-2.compute.amazonaws.com',
-    'ec2-54-201-151-221.us-west-2.compute.amazonaws.com',
-    'ec2-54-201-151-220.us-west-2.compute.amazonaws.com',
-    'ec2-54-201-151-166.us-west-2.compute.amazonaws.com',
-    'ec2-54-201-151-173.us-west-2.compute.amazonaws.com'
+    'ec2-54-213-49-129.us-west-2.compute.amazonaws.com',
+    'ec2-54-213-50-168.us-west-2.compute.amazonaws.com',
+    'ec2-54-201-84-45.us-west-2.compute.amazonaws.com',
+    'ec2-54-201-96-253.us-west-2.compute.amazonaws.com',
+    'ec2-54-213-50-237.us-west-2.compute.amazonaws.com',
+    'ec2-54-213-42-22.us-west-2.compute.amazonaws.com',
+    'ec2-54-201-156-10.us-west-2.compute.amazonaws.com',
+    'ec2-54-201-173-208.us-west-2.compute.amazonaws.com'
 ]
 
 // TOPOLOGY
