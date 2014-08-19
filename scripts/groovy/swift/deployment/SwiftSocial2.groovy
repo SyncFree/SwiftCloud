76ae9b2da15e9ece5b112e5b59a6dcac291ad136
Marek Zawirski <marek.zawirski@gmail.com>
2014-08-20 01:57:19 +0200

    SwiftSocial2: adjust target throghput by number of scouts

--- scripts/groovy/swift/deployment/SwiftSocial2.groovy
+++ scripts/groovy/swift/deployment/SwiftSocial2.groovy
@@ -73,7 +73,7 @@ class SwiftSocial2 extends SwiftBase {
     // Low level think time between each operation.
     def thinkTime = 0
     // If set, takes priority over think time:
-    def targetOpsPerSec = -1
+    def targetGlobalOpsPerSec = -1
     def baseWorkload = DEFAULT_WORKLOAD
     def recordPageViews = true
 
@@ -87,7 +87,7 @@ class SwiftSocial2 extends SwiftBase {
     protected void generateConfig() {
         def workload = baseWorkload + ['swiftsocial.numUsers':dbSize.toString(),
             'swiftsocial.thinkTime': thinkTime.toString(),
-            'swiftsocial.targetOpsPerSec' : targetOpsPerSec.toString(),
+            'swiftsocial.targetOpsPerSec' : ((Integer) (targetGlobalOpsPerSec / scouts())).toString(),
             'swiftsocial.recordPageViews': recordPageViews.toString()
         ]
         swiftSocialProps = DEFAULT_PROPS + workload + ['swift.reports' : reports.join(',')] + mode
