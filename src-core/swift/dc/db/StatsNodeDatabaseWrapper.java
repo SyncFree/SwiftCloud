22af85999cd1ba5ea713e84fb6505bf31e93094f
Marek Zawirski <marek.zawirski@gmail.com>
2014-08-20 20:00:25 +0200

    Extract DatabaseSizeStats as a separate component independent of DC code

--- src-core/swift/dc/db/StatsNodeDatabaseWrapper.java
+++ src-core/swift/dc/db/StatsNodeDatabaseWrapper.java
@@ -1,54 +1,51 @@
+/*****************************************************************************
+ * Copyright 2014 INRIA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
 package swift.dc.db;
 
-import java.util.Map.Entry;
 import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import swift.crdt.core.CRDTIdentifier;
 import swift.crdt.core.ManagedCRDT;
 import swift.dc.CRDTData;
 import swift.proto.MetadataStatsCollector;
+import swift.utils.DatabaseSizeStats;
 import swift.utils.SafeLog.ReportType;
-import sys.scheduler.PeriodicTask;
-
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Output;
 
 /**
- * DCNodeDatabase wrapper that records statistics on the database.
+ * DCNodeDatabase wrapper that records statistics on the database. Enabled with
+ * {@link ReportType#DATABASE_TABLE_SIZE}.
+ * <p>
+ * Thread unsafe (assumes external synchronization) for concurrent updates of
+ * the same object.
  * 
  * @author mzawirski
  */
 public class StatsNodeDatabaseWrapper implements DCNodeDatabase {
-    public static final String DB_STATS_REPORT_PERIOD_SEC_PROP_NAME = "swift.dbstats.reportPeriodSec";
-    public static final String DEFAULT_STATS_REPORT_PERIOD_SEC = "1.0";
-    private final MetadataStatsCollector statsCollector;
-    private final ConcurrentHashMap<String, TableStats> tablesStats = new ConcurrentHashMap<>();
-    private final AtomicInteger totalSize = new AtomicInteger();
     private final DCNodeDatabase wrappedDb;
+    private final DatabaseSizeStats stats;
 
     public StatsNodeDatabaseWrapper(final DCNodeDatabase wrappedDb, final String dcId) {
         this.wrappedDb = wrappedDb;
-        this.statsCollector = new MetadataStatsCollector(dcId);
+        this.stats = new DatabaseSizeStats(new MetadataStatsCollector(dcId));
     }
 
     @Override
     public void init(Properties props) {
+        stats.init(props);
         wrappedDb.init(props);
-        final double periodSec = Double.parseDouble(props.getProperty(DB_STATS_REPORT_PERIOD_SEC_PROP_NAME,
-                DEFAULT_STATS_REPORT_PERIOD_SEC));
-        if (statsCollector.isDatabaseTableReportEnabled()) {
-            new PeriodicTask(0.0, periodSec) {
-                // TODO: DCNodeDatabase does not offer any clean way to stop().
-                public void run() {
-                    statsCollector.recordDatabaseTableStats("ALL", totalSize.get());
-                    for (final Entry<String, TableStats> entry : tablesStats.entrySet()) {
-                        statsCollector.recordDatabaseTableStats(entry.getKey(), entry.getValue().getSize());
-                    }
-                };
-            };
-        }
     }
 
     // ASSUMPTION: no concurrent write(i1, ...) write(i2, ...) for i1 == i2
@@ -57,39 +54,9 @@ public class StatsNodeDatabaseWrapper implements DCNodeDatabase {
     @Override
     public boolean write(CRDTIdentifier id, CRDTData<?> data) {
         final boolean wrappedValue = wrappedDb.write(id, data);
-
-        if (ReportType.DATABASE_TABLE_SIZE.isEnabled()) {
-            // Optimistic concurrency control optimized for get().
-            TableStats tableStats = tablesStats.get(id.getTable());
-            if (tableStats == null) {
-                final TableStats existingStats = tablesStats.putIfAbsent(id.getTable(), tableStats = new TableStats());
-                if (existingStats != null) {
-                    tableStats = existingStats;
-                }
-            }
-            tableStats.updateObject(data.getCrdt());
-        }
+        final ManagedCRDT<?> crdt = data.getCrdt();
+        stats.updateObject(id, crdt);
         return wrappedValue;
-    }
-
-    private class TableStats {
-        private final AtomicInteger tableSize = new AtomicInteger();
-        private final ConcurrentHashMap<String, Integer> keySizes = new ConcurrentHashMap<>();
-
-        public int getSize() {
-            return tableSize.get();
-        }
-
-        public void updateObject(final ManagedCRDT crdt) {
-            final int newSize = computeSize(crdt);
-            Integer oldSize = keySizes.put(crdt.getUID().getKey(), newSize);
-            if (oldSize == null) {
-                oldSize = 0;
-            }
-            final int deltaSize = newSize - oldSize;
-            tableSize.addAndGet(deltaSize);
-            totalSize.addAndGet(deltaSize);
-        }
     }
 
     @Override
@@ -115,12 +82,5 @@ public class StatsNodeDatabaseWrapper implements DCNodeDatabase {
     @Override
     public boolean writeSysData(String table, String key, Object data) {
         return wrappedDb.writeSysData(table, key, data);
-    }
-
-    private int computeSize(ManagedCRDT crdt) {
-        final Kryo kryo = statsCollector.getFreshKryo();
-        final Output buffer = statsCollector.getFreshKryoBuffer();
-        crdt.write(kryo, buffer);
-        return buffer.position();
     }
 }
---
+++ src-core/swift/utils/DatabaseSizeStats.java
@@ -0,0 +1,134 @@
+/*****************************************************************************
+ * Copyright 2014 INRIA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+package swift.utils;
+
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import swift.crdt.core.CRDTIdentifier;
+import swift.crdt.core.ManagedCRDT;
+import swift.proto.MetadataStatsCollector;
+import swift.utils.SafeLog.ReportType;
+import sys.scheduler.PeriodicTask;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Output;
+
+/**
+ * Statistics of object database size, aggregated by table names. Enabled with
+ * {@link ReportType#DATABASE_TABLE_SIZE}.
+ * <p>
+ * Thread unsafe (assumes external synchronization) for concurrent updates of
+ * the same object.
+ * 
+ * @author mzawirski
+ */
+public class DatabaseSizeStats {
+    public static final String DB_STATS_REPORT_PERIOD_SEC_PROP_NAME = "swift.dbReportPeriodSec";
+    public static final String DEFAULT_STATS_REPORT_PERIOD_SEC = "1.0";
+    private final MetadataStatsCollector statsCollector;
+    private final ConcurrentHashMap<String, TableStats> tablesStats = new ConcurrentHashMap<>();
+    private final AtomicInteger totalSize = new AtomicInteger();
+
+    public DatabaseSizeStats(final MetadataStatsCollector statsCollector) {
+        this.statsCollector = statsCollector;
+    }
+
+    public void init(Properties props) {
+        final double periodSec = Double.parseDouble(props.getProperty(DB_STATS_REPORT_PERIOD_SEC_PROP_NAME,
+                DEFAULT_STATS_REPORT_PERIOD_SEC));
+        if (statsCollector.isDatabaseTableReportEnabled()) {
+            new PeriodicTask(0.0, periodSec) {
+                // TODO: implement stop()
+                public void run() {
+                    statsCollector.recordDatabaseTableStats("ALL", totalSize.get());
+                    for (final Entry<String, TableStats> entry : tablesStats.entrySet()) {
+                        statsCollector.recordDatabaseTableStats(entry.getKey(), entry.getValue().getSize());
+                    }
+                };
+            };
+        }
+    }
+
+    /**
+     * Informs of object removal.
+     * 
+     * @param id
+     */
+    public void removeObject(CRDTIdentifier id) {
+        updateObject(id, null);
+    }
+
+    /**
+     * Informs of object update that could have potentially caused changes in
+     * its size.
+     * 
+     * @param id
+     *            object id
+     * @param crdt
+     *            object
+     */
+    public void updateObject(CRDTIdentifier id, final ManagedCRDT<?> crdt) {
+        if (ReportType.DATABASE_TABLE_SIZE.isEnabled()) {
+            // Optimistic concurrency control optimized for get().
+            TableStats tableStats = tablesStats.get(id.getTable());
+            if (tableStats == null) {
+                final TableStats existingStats = tablesStats.putIfAbsent(id.getTable(), tableStats = new TableStats());
+                if (existingStats != null) {
+                    tableStats = existingStats;
+                }
+            }
+            tableStats.updateObject(id.getKey(), crdt);
+        }
+    }
+
+    private class TableStats {
+        private final AtomicInteger tableSize = new AtomicInteger();
+        private final ConcurrentHashMap<String, Integer> keySizes = new ConcurrentHashMap<>();
+
+        public int getSize() {
+            return tableSize.get();
+        }
+
+        public void updateObject(final String key, final ManagedCRDT crdt) {
+            final int newSize;
+            Integer oldSize;
+            if (crdt == null) {
+                newSize = 0;
+                oldSize = keySizes.remove(key);
+            } else {
+                newSize = computeSize(crdt);
+                oldSize = keySizes.put(key, newSize);
+            }
+            if (oldSize == null) {
+                oldSize = 0;
+            }
+            final int deltaSize = newSize - oldSize;
+            tableSize.addAndGet(deltaSize);
+            totalSize.addAndGet(deltaSize);
+        }
+    }
+
+    private int computeSize(ManagedCRDT crdt) {
+        final Kryo kryo = statsCollector.getFreshKryo();
+        final Output buffer = statsCollector.getFreshKryoBuffer();
+        crdt.write(kryo, buffer);
+        return buffer.position();
+    }
+
+}
