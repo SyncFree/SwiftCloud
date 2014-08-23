c58354a1ce1b9606d77231f80a4d7babd08ddde0
Marek Zawirski <marek.zawirski@gmail.com>
2014-08-23 13:22:08 +0200

    Fix SwiftSocial2 typo

--- scripts/groovy/swift/deployment/SwiftSocial2.groovy
+++ scripts/groovy/swift/deployment/SwiftSocial2.groovy
@@ -104,7 +104,7 @@ class SwiftSocial2 extends SwiftBase {
     protected void generateConfig() {
         def workload = baseWorkload + ['swiftsocial.numUsers':dbSize.toString(),
             'swiftsocial.thinkTime': thinkTime.toString(),
-            'swiftsocial.targetOpsPerSec' : ((Integer) (incomingOpPerSecLimit / scouts().size())).toString(),
+            'swiftsocial.targetOpsPerSec' : ((Integer) (incomingOpPerSecLimit / scouts.size())).toString(),
         ]
         swiftSocialProps = DEFAULT_PROPS + workload + ['swift.reports' : reports.join(',')] + mode
         swiftSocialPropsPath = "swiftsocial.properties"
